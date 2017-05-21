0e8ace7d7e73ab333693fa1839e836b54cdcf0d6
Sina Sabet <sinasabet81@gmail.com>
2017-05-21 00:20:26 -0400

    Added download link for each recording

--- src/AudioRecording.vue
+++ src/AudioRecording.vue
@@ -2,9 +2,9 @@
 <div class="audio_recording_root" >
   <audio class="recorded_audio" controls controlslist="download" preload="auto">
     <source :src="url" type="audio/webm"></source>
-    <a :href="url" :download="recnum + '.webm'">Direct Link</a>
     Your browser doesn't support the audio element :(
   </audio>
+  <a :href="url" :download="recnum + '.webm'">Download</a>
 </div>
 </template>
 
